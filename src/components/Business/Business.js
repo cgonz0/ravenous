0b9f985a3d7ed4f37ad88782884e1d94269f2c19
cgonz0 <cgonzadesign@gmail.com>
2019-02-06 22:05:52 -0800

    update styles

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -9,6 +9,7 @@ class Business extends React.Component {
           {/* accessed information via the business prop set in businesslist.js */}
           <img src={this.props.business.imageSrc} alt=''/>
         </div>
+        <h3 className="category">{this.props.business.category.toUpperCase()}</h3>
         <h2>{this.props.business.name}</h2>
         <div className="Business-information">
           <div className="Business-address">
@@ -17,7 +18,6 @@ class Business extends React.Component {
             <p>{`${this.props.business.state} ${this.props.business.zipCode}`}</p>
           </div>
           <div className="Business-reviews">
-            <h3>{this.props.business.category.toUpperCase()}</h3>
             <h3 className="rating">{`${this.props.business.rating} stars`}</h3>
             <p>{`${this.props.business.reviewCount} reviews`}</p>
           </div>
