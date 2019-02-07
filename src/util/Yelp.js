9474078da9d59b1ad84d5ee09b71882ca6eef8d7
cgonz0 <cgonzadesign@gmail.com>
2019-02-06 21:35:39 -0800

    required request header

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -7,7 +7,8 @@ const Yelp = {
     //return a promise that will ultimately resolve to our list of businesses
     return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
       headers: {
-        Authorization: `Bearer ${apiKey}`
+        "Authorization": `Bearer ${apiKey}`,
+        "X-Requested-With": XMLHttpRequest
       }
     }).then(response => {
       return response.json();
